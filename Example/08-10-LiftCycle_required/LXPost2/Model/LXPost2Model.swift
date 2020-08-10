c8fbc40eb41cb3717e5e18d21913d77d90e7af0e
lxthyme <1094426094@qq.com>
2020-08-10 18:40:11 +0800

    update

---
+++ Example/08-10-LiftCycle_required/LXPost2/Model/LXPost2Model.swift
@@ -0,0 +1,16 @@
+//
+//  LXPost2Model.swift
+//  LXToolKit_Example
+//
+//  Created by LXThyme Jason on 2020/8/10.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+import Foundation
+import HandyJSON
+
+class LXPost2Model: NSObject, HandyJSON {
+    required override init() {}
+
+    // func mapping(mapper: HelpingMapper) {}
+    // override var debugDescription: String { return "" }
+}
