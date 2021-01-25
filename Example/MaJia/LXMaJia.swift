09b797b4b8312a4299c0249f268c904c08e8cfb7
lxthyme <1094426094@qq.com>
2021-01-25 14:16:55 +0800

    LXMaJia

---
+++ Example/MaJia/LXMaJia.swift
@@ -0,0 +1,13 @@
+//
+//  LXMaJia.swift
+//  MaJia
+//
+//  Created by LXThyme Jason on 2021/1/25.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+class LXMaJia {
+    static let fm = FileManager.default
+}
