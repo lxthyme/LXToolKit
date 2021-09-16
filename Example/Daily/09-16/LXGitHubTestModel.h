61b82998cb49e49fd87c7f678ef42ee837cd7f13
lxthyme <lx314333@gmail.com>
2021-09-16 16:24:54 +0800

    fixed

---
+++ Example/Daily/09-16/LXGitHubTestModel.h
@@ -0,0 +1,18 @@
+//
+//  LXGitHubTestModel.h
+//  LXToolKit_Exam
+//
+//  Created by lxthyme on 2021/9/16.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LXGitHubTestModel : NSObject {
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/Daily/09-16/LXGitHubTestModel.m
@@ -0,0 +1,24 @@
+//
+//  LXGitHubTestModel.m
+//  LXToolKit_Exam
+//
+//  Created by lxthyme on 2021/9/16.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+#import "LXGitHubTestModel.h"
+
+@interface LXGitHubTestModel() {
+}
+
+@end
+
+@implementation LXGitHubTestModel
+
+#pragma mark -
+#pragma mark - 👀Public Actions
+
+#pragma mark -
+#pragma mark - 🔐Private Actions
+
+@end
