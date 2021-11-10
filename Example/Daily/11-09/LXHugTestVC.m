9327906866a26e5e9591b030f89178983c082aff
lxthyme <lx314333@gmail.com>
2021-11-10 16:08:18 +0800

    feat: update

--- Example/Daily/11-09/LXHugTestVC.m
+++ Example/Daily/11-09/LXHugTestVC.m
@@ -21,22 +21,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
     [self prepareUI];
 }
 
 - (void)prepareUI {
     [self.view addSubview:self.leftView];
     [self.view addSubview:self.labelRight];
+
+    [self masonry];
 }
 
 - (void)masonry {
+    [self.leftView setContentHuggingPriority:UILayoutPriorityDefaultLow forAxis:UILayoutConstraintAxisHorizontal];
+    [self.leftView setContentCompressionResistancePriority:UILayoutPriorityDefaultLow forAxis:UILayoutConstraintAxisHorizontal];
+    [self.labelRight setContentHuggingPriority:UILayoutPriorityDefaultHigh forAxis:UILayoutConstraintAxisHorizontal];
+    [self.labelRight setContentCompressionResistancePriority:UILayoutPriorityDefaultHigh forAxis:UILayoutConstraintAxisHorizontal];
     [self.leftView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(@100.f);
-        make.left.greaterThanOrEqualTo(@16.f);
+        make.left.equalTo(@16.f);
         make.height.equalTo(@44.f);
     }];
     [self.labelRight mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.equalTo(self.leftView.mas_right).offset(30.f);
+        make.left.equalTo(self.leftView.mas_right);
         make.right.equalTo(@(-15.f));
         make.centerY.equalTo(self.leftView);
     }];
@@ -52,7 +59,8 @@
 - (UILabel *)labelRight {
     if(!_labelRight){
         _labelRight = [[UILabel alloc]init];
-        _labelRight.text = @"233";
+        _labelRight.text = @"233233233233";
+        _labelRight.backgroundColor = [UIColor cyanColor];
     }
     return _labelRight;
 }
