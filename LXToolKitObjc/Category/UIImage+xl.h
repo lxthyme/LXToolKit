aadbf728ca55bd6f3e70f8016ac4bf6fb0cd8e3b
lxthyme <lx314333@gmail.com>
2022-06-21 09:47:04 +0800

    feat: update

---
+++ LXToolKitObjc/Category/UIImage+xl.h
@@ -0,0 +1,18 @@
+//
+//  UIImage+xl.h
+//  LXToolKitObjc
+//
+//  Created by lxthyme on 2022/6/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIImage (xl)
+
++ (UIImage *)xl_imageNamed:(NSString *)name;
+
+@end
+
+NS_ASSUME_NONNULL_END
