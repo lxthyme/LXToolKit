3dd275301e9ea101259edeb6aa5abf9f5382716b
lxthyme <lx314333@gmail.com>
2022-06-29 13:41:59 +0800

    feat: update

--- LXToolKitObjc/Base/Model/LXBaseModel.h
+++ LXToolKitObjc/Base/Model/LXBaseModel.h
@@ -6,10 +6,17 @@
 //
 #import "LXBaseObject.h"
 
+static NSString * const _Nonnull kIgnorePropertyPreffix = @"f_";
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface LXBaseModel : NSObject {
 }
+
+/// 当 JSON 转为 Model 完成后，该方法会被调用。
+/// 你可以在这里对数据进行校验，如果校验不通过，可以返回 NO，则该 Model 会被忽略。
+/// 你也可以在这里做一些自动转换不能完成的工作。
+- (void)prepareData;
 
 @end
 
--- LXToolKitObjc/Base/Model/LXBaseModel.m
+++ LXToolKitObjc/Base/Model/LXBaseModel.m
@@ -6,6 +6,9 @@
 //
 #import "LXBaseModel.h"
 
+#import <ReactiveObjC/ReactiveObjC.h>
+#import <YYModel/YYModel.h>
+
 @interface LXBaseModel() {
 }
 
@@ -14,7 +17,38 @@
 @implementation LXBaseModel
 
 #pragma mark -
+#pragma mark - 🛠Life Cycle
+/// 如果实现了该方法，则处理过程中会忽略该列表内的所有属性
++ (NSArray *)modelPropertyBlacklist {
+    YYClassInfo *a = [YYClassInfo classInfoWithClass:[self class]];
+    return [a.propertyInfos.allKeys.rac_sequence filter:^BOOL(NSString *_Nullable key) {
+        return [key hasPrefix:kIgnorePropertyPreffix];
+    }].array;
+}
+
+/// 如果实现了该方法，则处理过程中不会处理该列表外的属性。
+// + (NSArray *)modelPropertyWhitelist {
+//     return @[@"name1"];
+// }
+
+/// 当 JSON 转为 Model 完成后，该方法会被调用。
+/// 你可以在这里对数据进行校验，如果校验不通过，可以返回 NO，则该 Model 会被忽略。
+/// 你也可以在这里做一些自动转换不能完成的工作。
+- (BOOL)modelCustomTransformFromDictionary:(NSDictionary *)dic {
+    [self prepareData];
+    return YES;
+}
+
+/// 当 Model 转为 JSON 完成后，该方法会被调用。
+/// 你可以在这里对数据进行校验，如果校验不通过，可以返回 NO，则该 Model 会被忽略。
+/// 你也可以在这里做一些自动转换不能完成的工作。
+- (BOOL)modelCustomTransformToDictionary:(NSMutableDictionary *)dic {
+    return YES;
+}
+
+#pragma mark -
 #pragma mark - 👀Public Actions
+- (void)prepareData {}
 
 #pragma mark -
 #pragma mark - 🔐Private Actions
