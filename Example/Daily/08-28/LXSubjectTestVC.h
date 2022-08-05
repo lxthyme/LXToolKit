5cce690f7eb08feab3c54e3e58deba77b1732391
lxthyme <lx314333@gmail.com>
2022-08-05 22:38:59 +0800

    feat: 替换为 DaoJia 路径

--- Example/Daily/08-28/LXSubjectTestVC.h
+++ Example/Daily/08-28/LXSubjectTestVC.h
@@ -1,15 +1,15 @@
 //
-//  DJClassifyVC.h
+//  LXSubjectTestVC.h
 //  LXToolKitObjc_Example
 //
-//  Created by lxthyme on 2022/6/19.
+//  Created by lxthyme on 2022/8/5.
 //  Copyright © 2022 lxthyme. All rights reserved.
 //
-#import <DJBusinessTools/LXBaseVC.h>
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface DJClassifyVC : LXBaseVC {
+@interface LXSubjectTestVC: UIViewController {
 }
 
 @end
---
+++ Example/Daily/08-28/LXSubjectTestVC.m
@@ -0,0 +1,74 @@
+//
+//  LXSubjectTestVC.m
+//  LXToolKitObjc_Example
+//
+//  Created by lxthyme on 2022/8/5.
+//  Copyright © 2022 lxthyme. All rights reserved.
+//
+#import "LXSubjectTestVC.h"
+
+#import <Masonry/Masonry.h>
+
+@interface LXSubjectTestVC() {
+}
+
+@end
+
+@implementation LXSubjectTestVC
+- (void)dealloc {
+    NSLog(@"🛠DEALLOC: %@", NSStringFromClass([self class]));
+}
+
+#pragma mark -
+#pragma mark - 🛠Life Cycle
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:YES];
+    // NSLog(@"🛠viewWillAppear: %@", NSStringFromClass([self class]));
+}
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:YES];
+    // NSLog(@"🛠viewDidAppear: %@", NSStringFromClass([self class]));
+}
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:YES];
+    // NSLog(@"🛠viewWillDisappear: %@", NSStringFromClass([self class]));
+}
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:YES];
+    // NSLog(@"🛠viewDidDisappear: %@", NSStringFromClass([self class]));
+}
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // NSLog(@"🛠viewDidLoad: %@", NSStringFromClass([self class]));
+    // Do any additional setup after loading the view.
+
+    [self prepareUI];
+}
+
+#pragma mark -
+#pragma mark - 🌎LoadData
+
+#pragma mark -
+#pragma mark - 👀Public Actions
+
+#pragma mark -
+#pragma mark - 🔐Private Actions
+
+#pragma mark -
+#pragma mark - 🍺UI Prepare & Masonry
+- (void)prepareUI {
+    self.view.backgroundColor = [UIColor whiteColor];
+
+    // [self.<#view#> addSubview:self.<#table#>];
+
+    [self masonry];
+}
+
+#pragma mark Masonry
+- (void)masonry {
+    // MASAttachKeys(<#...#>)
+}
+
+#pragma mark Lazy Property
+
+@end
