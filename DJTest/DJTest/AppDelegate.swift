9a6ef700deea3760bccf79a2e4058a6215e95117
lxthyme <lx314333@gmail.com>
2023-12-22 18:05:44 +0800

    feat: flutter support

--- DJTest/DJTest/AppDelegate.swift
+++ DJTest/DJTest/AppDelegate.swift
@@ -10,10 +10,10 @@ import Flutter
 import FlutterPluginRegistrant
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    lazy var flutterEngine = FlutterEngine(name: "")
+class AppDelegate: FlutterAppDelegate {
+    lazy var flutterEngine = FlutterEngine(name: "flutter_cookbook")
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         // Use the Firebase library to configure APIs.
         LXLibManager.setupLibs()
@@ -24,13 +24,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    override func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    override func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
