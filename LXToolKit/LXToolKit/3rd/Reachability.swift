b70da1629ecc2667e17928af5d5ecf50717cb985
lxthyme <lx314333@gmail.com>
2023-12-29 15:46:15 +0800

    feat: update

--- LXToolKit/LXToolKit/3rd/Reachability.swift
+++ LXToolKit/LXToolKit/3rd/Reachability.swift
@@ -7,10 +7,8 @@
 
 import Foundation
 import Alamofire
-import RxSwift
 
-// An observable that completes when the app gets online (possibly completes immediately).
-public func connectedToInternet() -> Observable<Bool> {
+func connectedToInternet() -> Observable<Bool> {
     return ReachabilityManager.shared.reach
 }
 
