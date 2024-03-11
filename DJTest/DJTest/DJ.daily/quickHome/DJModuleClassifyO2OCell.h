9165a3158c4a6b0f61019c522c4186e56b659bbb
lxthyme <lx314333@gmail.com>
2024-03-11 13:47:43 +0800

    feat: 极速版分类页组件

---
+++ DJTest/DJTest/DJ.daily/quickHome/DJModuleClassifyO2OCell.h
@@ -0,0 +1,20 @@
+//
+//  DJModuleClassifyO2OCell.h
+//  DJTest
+//
+//  Created by lxthyme on 2024/1/31.
+//
+#import <UIKit/UIKit.h>
+#import <DJBusinessModule/DJClassifyQuicklyO2OVC.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DJModuleClassifyO2OCell: DJBaseTableViewCell {
+}
+@property(nonatomic, strong)DJClassifyQuicklyO2OVC *classifyO2OVC;
+
+- (void)dataFill:(DJClassifyQuickly *)quicklyModel;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- DJTest/DJTest/DJ.daily/quickHome/DJQuickHomeVC.h
+++ DJTest/DJTest/DJ.daily/quickHome/DJQuickHomeVC.h
@@ -8,7 +8,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface DJQuickHomeVC: UIViewController {
+@interface DJQuickHomeVC: DJBaseVC {
 }
 
 @end
