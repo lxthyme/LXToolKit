b31f2f53eb75d23457b525e41e2c880523ccbf48
lxthyme <lx314333@gmail.com>
2024-04-04 14:47:37 +0800

    feat: xcconfig update

--- DJTest/Configuration/Common/version.commit.sh
+++ DJTest/Configuration/Common/version.commit.sh
@@ -13,7 +13,7 @@ build_version=$number_of_commits
 fi
 
 key_version='MARKETING_VERSION'
-file_config='Common.xcconfig'
+file_config='bundle_identifier.xcconfig'
 previous_build_number=$(awk -F "=" '/'$key_version' = / {print $2}' $file_config | tr -d ' ')
 echo "-->build version[$CONFIGURATION]: $previous_build_number -> $build_version"
 
